06378d235bd20760acf069a506c56d860e2c71d6
tevin08 <estevaopsg08@gmail.com>
2023-09-20 23:43:52 -0300

    nextjs-blog

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,19 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Alert from '../components/alert';
 
+
+
 export default function Home() {
   return (
-    <Layout home>
+    
+    <body  className=' bg-gray-600  h-screen'>
+        <Layout home>
+
       <Head>
         <title> My Profile </title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, My name is Estêvão i am 17 years old, I live in São Vicente </p>
+        <p className='text-red-500'>Hello, My name is Estêvão i am 17 years old, I live in São Vicente </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -18,6 +23,7 @@ export default function Home() {
         <Alert type='success' >Sucesso</Alert>
       </section>
     </Layout>
+      </body>
   );
 }
 
